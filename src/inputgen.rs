51a504e0ef9af1ac2343225bafdb66755970499d
bamgoesn <60645387+BamgoeSN@users.noreply.github.com>
2022-09-27 23:46:42 +0900

    Add allow unused

--- src/inputgen.rs
+++ src/inputgen.rs
@@ -1,10 +1,11 @@
-use rand::{thread_rng, Rng};
+#![allow(unused)]
+
+use rand::{seq::SliceRandom, thread_rng, Rng};
 use std::{fmt::Write, future::Future};
 use tokio::task::JoinError;
 
-use crate::core::string::process_str;
+use crate::{core::string::process_str, genhelper::tree_gen::Tree};
 
-#[allow(unused)]
 pub async fn generate_input() -> String {
     let mut buf = String::new();
     let mut rng = thread_rng();
@@ -12,13 +13,8 @@ pub async fn generate_input() -> String {
     macro_rules! out { ($($arg:tt)*) => { write!(buf, $($arg)*).unwrap(); }; }
     macro_rules! outln { ($($arg:tt)*) => { writeln!(buf, $($arg)*).unwrap(); }; }
 
-    let n: usize = rng.gen_range(1..=5);
-    outln!("{n}");
-    for _ in 0..n {
-        let start: u32 = rng.gen_range(0..=1000);
-        let end: u32 = rng.gen_range(start..=1000);
-        outln!("{start} {end}");
-    }
+    let n: usize = rng.gen_range(1..=50000);
+    outln!("{}", n);
 
     process_str(&buf)
 }
