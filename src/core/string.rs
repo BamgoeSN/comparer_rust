503e8ef23a31762c0d665efce50abf701006c039
BamgoeSN <60645387+BamgoeSN@users.noreply.github.com>
2023-07-16 07:32:09 +0900

    Change process_str

--- src/core/string.rs
+++ src/core/string.rs
@@ -3,7 +3,11 @@ use std::fmt::Write;
 pub fn process_str(s: &str) -> String {
     let mut builder = String::new();
     for l in s.lines() {
-        writeln!(builder, "{}", l.trim()).unwrap();
+        writeln!(builder, "{}", l.trim_end()).unwrap();
+    }
+    let trimmed_len = builder.trim_end().len();
+    while builder.len() > trimmed_len + 1 {
+        builder.pop();
     }
     builder
 }
